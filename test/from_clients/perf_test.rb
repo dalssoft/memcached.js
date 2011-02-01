75e9ffbea5411a0becacd3773442ef00d8372c23
David Lojudice Sobrinho <dalssoft@gmail.com>
2011-02-01 14:13:03 -0200

    perf test

---
+++ test/from_clients/perf_test.rb
@@ -0,0 +1,34 @@
+
+require 'rubygems'
+require 'memcache'
+
+memcache = MemCache.new('localhost:11211')
+
+n = 800
+value = ""
+(1048000).times do |x| value << ('.') end
+
+n.times do |x|
+
+  key = "big_" + x.to_s
+  
+  #puts key, value.length
+  memcache.set key, value, 100000
+  
+  if x % 20 == 0
+    start = Time.now
+    
+    key_test = "smail_" + x.to_s
+    value_test = "small value"
+    memcache.set key_test, value_test, 100000
+    memcache.get key_test
+    
+    elapsed = Time.now - start
+    puts "#{x}|#{elapsed}"
+    #puts key_test, value_test.length
+  end
+  
+  #result1 = memcache.get key
+  #puts "Erro!" if result1[0] != value
+
+end
